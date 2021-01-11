898f2944149834cce577e022a1bb507d512eeb5e
Abrahan Nevarez <ab260261@CDLC02R91X1FVH6.local>
2021-01-11 12:56:57 -0500

    Finished the github adding info portion of the site

--- js/projectPull.js
+++ js/projectPull.js
@@ -6,30 +6,61 @@ class Project {
     this.forks = 0;
     this.url = "";
     this.homepage = "";
-    this.content = "";
     this.readme_picture = "";
     this.cloudTech = "";
     this.responsibilities = "";
+    this.technology = "";
+    this.status = "";
+    this.deployedLink = "";
   }
 }
+/*
+Given the parent element, append the child to the parent
+*/
 function listAppend(parent, element){
   return parent.appendChild(element);
 }
-function nodeCreator(element){
-  return document.createElement(element);
+/*
+Given an element and text, create a node and return that node to be appended
+*/
+function buttonCreator(link, text){
+  var button = document.createElement("button");
+  if(link != "None"){
+    var linkText = document.createElement("a");
+    linkText.setAttribute("href", link);
+    linkText.textContent = text;
+    button.style.opacity = "1";
+    button.appendChild(linkText);
+  }else{
+    var link = document.createElement("a");
+    button.style.opacity = "0";
+  }
+  return button;
 }
 /*
-Given a keyword, search for it within the array and returns the string
+Creates a p tag and decides if it's going to be visible or not
 */
-function keywordFinder(keyword) {
-
+function textCreator(attribute, text){
+  var textTag = document.createElement("p");
+  if(attribute != "None"){
+  var textData = document.createTextNode(text);
+  textTag.appendChild(textData);
+  textTag.style.opacity = "1";
+  }else{
+    var textData = document.createTextNode(text);
+    textTag.style.opacity = "0";
+  }
+  return textTag;
+}
+function nodeCreator(element){
+  return document.createElement(element);
 }
 
 /*
 Given a project name, fetch the data from the github api
 */
-function projectStats(projectName) {
-  document.getElementById("projects");
+function projectStats(projectName, cloudTech, responsibilities, description, technology, status, websiteURL) {
+  projects = document.getElementById("projects");
   projectData = new Project();
   fetch("https://api.github.com/repos/zenith110/" + projectName)
     .then(function (response) {
@@ -37,25 +68,44 @@ function projectStats(projectName) {
       // This is a weird quirk of Fetch
       return response.json();
     }).then(function (data) {
-      projectData.createdAt = data.created_at;
-      projectData.description = data.description;
+      var date = new Date(data.created_at);
+      projectData.deployedLink = websiteURL;
+      projectData.createdAt = date;
+      projectData.description = description;
       projectData.language = data.language;
       projectData.forks = data.forks;
       projectData.url = data.html_url;
       projectData.homepage = data.homepage;
-      projectData.readme_picture = "https://raw.githubusercontent.com/zenith110/Xatu_Bot/master/readme_picture.png"
+      projectData.readme_picture = "https://raw.githubusercontent.com/zenith110/" + projectName + "/master/readme_picture.png"
+      projectData.cloudTech = cloudTech;
+      projectData.responsibilities = responsibilities;
+      projectData.homepage = data.html_url;
+      projectData.technology = technology;
+      projectData.status = status;
       let li = nodeCreator("li");
       let img = nodeCreator("img");
-      let span = nodeCreator("span")
+      let descriptionData = textCreator(projectData.description, "Description: " + projectData.description);
+      let responsibilitiesData = textCreator(projectData.responsibilities, "Responsibilites on the project:\n" + projectData.responsibilities);
+      let githubButton = buttonCreator(projectData.url, "Github repo");
+      let cloudTechData = textCreator(projectData.cloudTech, "Used the following cloud tech:\n" + projectData.cloudTech);
+      let createdAtData = textCreator(projectData.createdAt, "Created on: " + projectData.createdAt.getMonth() + "/" + projectData.createdAt.getDate() + "/" + projectData.createdAt.getFullYear());
+      let technologyData = textCreator(projectData.technology, "Technology used were:\n" + projectData.technology);
+      let statusData = textCreator(projectData.status, "Current status of this project: " + projectData.status);
+      let websiteData = buttonCreator(projectData.deployedLink, "Website link");
       img.src = projectData.readme_picture;
       listAppend(li, img);
+      listAppend(li, createdAtData);
+      listAppend(li, technologyData);
+      listAppend(li, statusData);
+      listAppend(li, cloudTechData);
+      listAppend(li, descriptionData);
+      listAppend(li, responsibilitiesData);
+      listAppend(li, githubButton);
+      listAppend(li, websiteData);
+      projects.appendChild(li)
+  
     }).catch(function (error) {
       // if there's an error, log it
       console.log(error);
     });
-    let li = nodeCreator("li");
-    let img = nodeCreator("img");
-    img.src = projectData.readme_picture;
-    listAppend(li, img);
-    return projectData;
 }
